560eb4048fd42a8fe7760f7d4839226ea2c36430
Npcldtt <142570475+Npcldtt@users.noreply.github.com>
2024-04-05 10:00:27 +0500

    Update

---
+++ GS.js
@@ -0,0 +1,18 @@
+$eva
+
+//
+// google search
+//
+// made by npxcd (voix)
+//
+// enable phrase
+//
+
+msg = "$messageAfterCommand"
+search = msg.split(" ").join("+")
+
+site = "https://www.google.com/search?q=" + search
+
+reply = "[" + msg + " - Google Search](" + site + ")"
+
+$halt $get(reply)
