bdc7cc7d74c61daee3813ce1b010f77293ea159f
Npcldtt <142570475+Npcldtt@users.noreply.github.com>
2024-07-07 13:04:33 +0500

    Update

---
+++ AFK2.js
@@ -0,0 +1,11 @@
+$eval
+
+// made by voixxx \\
+
+var afk$mentionedID = afk$mentionedID
+var message = ""
+
+if (afk$mentionedID != undefined) {
+    message = "$mentionedNamePlain is AFK:" + afk$mentionedID
+} $halt
+$get(message)
