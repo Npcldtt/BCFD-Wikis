bd37fb57aecc8fab3e16903ed39d777c3c19b332
Npcldtt <142570475+Npcldtt@users.noreply.github.com>
2024-07-07 13:03:51 +0500

    Update

---
+++ AFK.js
@@ -0,0 +1,25 @@
+$eval
+
+// made by voixxx \\
+
+var afk$ID = afk$ID
+var phrase = "$messageAfterCommand"
+var message
+
+cmd = "!afk" // your cmd here
+
+if (phrase == " remove" && afk$ID != undefined) {
+    message = "$name AFK Successfully removed"
+    afk$ID = undefined
+} else if (phrase == " remove" && afk$ID == undefined) {
+    message = "Cannot remove. You didn't set the AFK"
+} else if (phrase != " remove" && afk$ID == undefined) {
+    message = "$name set your AFK:" + phrase + "\n\nTo remove your AFK, type `" + cmd + " remove`"
+    afk$ID = phrase
+} else if (phrase != " remove" && afk$ID != undefined) {
+    message = "$name change your afk:" + phrase + "\n\nTo remove your AFK, type `" + cmd + " remove`"
+    afk$ID = phrase
+}
+
+$halt
+$get(message)
